6b7cc7071f5485d8eb27ad3b435ca42277fa2f7e
Jan Nowakowski <janeknowakowski9@gmail.com>
2025-02-17 12:32:07 +0100

    Add repro

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -5,114 +5,75 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {
+  Ref,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Input = ({}, ref: Ref<TextInput>) => {
+  const textInputRef = useRef(null);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  useImperativeHandle(
+    ref,
+    () => {
+      if (!textInputRef.current) {
+        throw Error('Ref is not ready');
+      }
+      return {
+        focus: () => {
+          textInputRef.current.focus();
+        },
+      };
+    },
+    [],
+  );
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  return <TextInput ref={textInputRef} placeholder="Text Input" />;
+};
+
+const InputWithRef = React.forwardRef(Input);
+
+const Parent = () => {
+  const ref = useRef(null);
+  const [showInput, setShowInput] = useState(false);
+
+  useEffect(() => {
+    if (showInput) {
+      ref.current.focus();
+    }
+  }, [showInput]);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{backgroundColor: 'green', padding: 20}}>
+      {showInput && <InputWithRef ref={ref} />}
+      <Button title="Toggle Input" onPress={() => setShowInput(p => !p)} />
     </View>
   );
-}
-
+};
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          backgroundColor: 'grey',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+        }}>
+        <Parent />
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
